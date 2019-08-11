0746b6d8d46d4f0742c3d5dd5aabcebd372255b6
Vijith B <vijubalak@gmail.com>
2019-08-11 12:36:47 +0530

    Array indexOf function example

---
+++ arrays/2. indexOf.js
@@ -0,0 +1,7 @@
+var items = ['first', 'second', 'third', 'fourth', 'fifth'];
+ 
+console.log(items.indexOf('fifth')); // 4
+console.log(items.indexOf('fourth')); // 3
+console.log(items.indexOf('third')); // 2
+console.log(items.indexOf('second')); // 1
+console.log(items.indexOf('first')); // 0
